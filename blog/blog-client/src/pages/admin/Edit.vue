8642687ed9b0f3c05f0b3370e347ce530c7aa6f7
icedvigilante <wm.ryan.mooney@gmail.com>
2021-10-31 16:05:19 -0800

    Update to allow slug to change and be tracked. Implement selectAll on the slug input when clicked.

--- blog/blog-client/src/pages/admin/Edit.vue
+++ blog/blog-client/src/pages/admin/Edit.vue
@@ -4,7 +4,8 @@
   <div>
     <div class="absolute w-full left-0 top-0 p-6 flex justify-between items-center space-x-6">
       <div class="flex-grow flex items-center">
-        <span class="mr-1">/</span> <input type="text" class="p-0 border-none focus:ring-0 w-full" v-model="post.slug">
+        <span class="mr-1">/</span> <input type="text" class="p-0 border-none focus:ring-0 w-full" v-model="post.slug"
+        spellcheck="false" @click="$event.target.select()">
       </div>
       <div class="flex items-center space-x-6">
         <div>
@@ -13,7 +14,9 @@
         <button class="text-sm font-medium">
           Published
         </button>
-        <router-link class="text-sm font-medium text-gray-800" :to="{name: 'post', params: {slug: post.slug} }">
+<!--        todo: fix the preview link.-->
+<!--        <router-link class="text-sm font-medium text-gray-800" :to="{name: 'post', params: {slug: post.slug}}">-->
+        <router-link class="text-sm font-medium text-gray-800" to="/">
           Preview
         </router-link>
       </div>
@@ -25,7 +28,7 @@
     <ResizeTextarea v-if="post.title">
       <template v-slot:default="{resize, el}">
       <textarea :ref="el" v-model="post.title" @input="resize" class="w-full text-4xl sm:text-6xl leading-10 font-extrabold tracking-tight
-      text-gray-900 text-center border-none focus:ring-0 resize-none p-0" rows="1"></textarea>
+      text-gray-900 text-center border-none focus:ring-0 resize-none p-0"></textarea>
       </template>
 
     </ResizeTextarea>
@@ -37,9 +40,10 @@
 
 <script>
 import useAdminPosts from "../../api/useAdminPosts";
-import {onMounted, watch} from "vue";
+import {onMounted, watch, watchEffect} from "vue";
 import _ from 'lodash';
 import ResizeTextarea from "../../components/ResizeTextarea.vue";
+import slugify from "slugify";
 
 export default {
   name: "Edit",
@@ -61,10 +65,18 @@ export default {
     }
 
 
-
+    const replaceSlug = () => {
+      const slug = post.value.slug
+      if(slug.charAt(slug.length - 1) == " "){
+        return
+      }
+      post.value.slug = slug ? slugify(slug, {strict:true}) : post.value.uuid
+    }
     onMounted(async () => {
       await fetchPost(props.uuid)
-
+      watchEffect(() => {
+        replaceSlug()
+      })
       // cloning the entire post using LowDash "_" and the cloneDeep function
       watch(() => _.cloneDeep(post), _.debounce(() => {
         updatePost()
